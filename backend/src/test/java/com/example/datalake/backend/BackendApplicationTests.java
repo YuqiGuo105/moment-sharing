a77c734334616b3ba32fcbb8bf2048a239b75d71
Yuqi Guo <131561736+YuqiGuo105@users.noreply.github.com>
2025-06-16 22:36:07 -0600

    Bug fix for CI/CD

--- backend/src/test/java/com/example/datalake/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/datalake/backend/BackendApplicationTests.java
@@ -1,9 +1,9 @@
 package com.example.datalake.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
     @Test
