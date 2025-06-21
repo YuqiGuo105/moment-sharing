5f6bb81486bfac9dfe8d9ad73f52657cfba9d223
Yuqi Guo <131561736+YuqiGuo105@users.noreply.github.com>
2025-06-21 16:50:23 -0600

    Add H2 dependency for tests

--- backend/src/test/resources/records.sql
+++ backend/src/test/resources/records.sql
@@ -1,3 +1,5 @@
+-- src/test/resources/records.sql
+TRUNCATE TABLE "Record";
 INSERT INTO "Record" (id, created_at, url, owner) VALUES
-('11111111-1111-1111-1111-111111111111', CURRENT_TIMESTAMP, 'http://example.com', 'alice'),
-('22222222-2222-2222-2222-222222222222', CURRENT_TIMESTAMP, 'http://example.org', 'bob');
+                                                      ('11111111-1111-1111-1111-111111111111', CURRENT_TIMESTAMP, 'http://example.com', 'alice'),
+                                                      ('22222222-2222-2222-2222-222222222222', CURRENT_TIMESTAMP, 'http://example.org', 'bob');
