0a92ad865e40f6a627bf714cd764b2807fb7a06f
Yuqi Guo <131561736+YuqiGuo105@users.noreply.github.com>
2025-06-23 22:48:11 -0600

    Fix SQL script for tests

--- backend/src/test/resources/sql/records.sql
+++ backend/src/test/resources/sql/records.sql
@@ -1,12 +1,4 @@
-CREATE TABLE IF NOT EXISTS "Record" (
-  id uuid not null default gen_random_uuid(),
-  created_at timestamp with time zone not null default now(),
-  url text null,
-  owner text null,
-  constraint Record_pkey primary key (id)
-);
-
-TRUNCATE TABLE "Record";
+DELETE FROM "Record";
 
 INSERT INTO "Record"(id, created_at, url, owner) VALUES
   ('11111111-1111-1111-1111-111111111111', NOW(), 'http://example.com/1', 'alice'),
