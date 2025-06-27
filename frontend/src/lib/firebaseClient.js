16125413eb1ff16ea79b2774e63093f503dcdfd0
Yuqi Guo <131561736+YuqiGuo105@users.noreply.github.com>
2025-06-27 09:06:22 -0600

    Improve auth UI and add Firebase storage upload

--- frontend/src/lib/firebaseClient.js
+++ frontend/src/lib/firebaseClient.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -14,3 +15,4 @@ const firebaseConfig = {
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firestore = getFirestore(firebaseApp);
 export const auth = getAuth(firebaseApp);
+export const storage = getStorage(firebaseApp);
