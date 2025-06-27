48b779948e649a5cce1f285db86f41359e554179
Yuqi Guo <131561736+YuqiGuo105@users.noreply.github.com>
2025-06-26 20:11:54 -0600

    Switch from Supabase to Firebase

--- backend/src/main/java/com/example/datalake/backend/dao/RecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.datalake.backend.dao;
-
-import com.example.datalake.backend.model.Record;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface RecordRepository extends JpaRepository<Record, UUID> {
-}
